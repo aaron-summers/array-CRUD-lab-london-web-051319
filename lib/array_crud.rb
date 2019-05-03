c0522a974b7b71539e9da3c62af824ff97773ef1
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 14:18:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  children_of_thanos.push("Nebula")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
