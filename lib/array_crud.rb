0486f1bfeebae563bd5df608c95b5393f77cb6f9
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 14:12:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  children_of_thanos = ["Ebony Mau"]
+  children_of_thanos = ["Ebony Maw", "Proxima Midnight", ""]
 end
 
 def add_element_to_end_of_array(array, element)
